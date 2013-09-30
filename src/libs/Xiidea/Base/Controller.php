6d7f797c7a22d9d25165877aad205d73fcb1bed4
Roni Saha <roni.cse@gmail.com>
2013-09-30 23:58:02 +0600

    Controller Common libraries type hint added

--- src/libs/Xiidea/Base/Controller.php
+++ src/libs/Xiidea/Base/Controller.php
@@ -5,6 +5,49 @@ namespace Xiidea\Base;
 use Xiidea\Twig\Dummy;
 use Xiidea\Twig\Loader;
 
+/**
+ * @property \CI_DB_active_record $db
+ * @property \CI_DB_forge $dbforge
+ * @property \CI_Benchmark $benchmark
+ * @property \CI_Calendar $calendar
+ * @property \CI_Cart $cart
+ * @property \CIX_Config $config
+ * @property \CIX_Controller $controller
+ * @property \CI_Email $email
+ * @property \CI_Encrypt $encrypt
+ * @property \CI_Exceptions $exceptions
+ * @property \CI_Form_validation $form_validation
+ * @property \CI_Ftp $ftp
+ * @property \CI_Hooks $hooks
+ * @property \CI_Image_lib $image_lib
+ * @property \CI_Input $input
+ * @property \CIX_Lang $lang
+ * @property \CIX_Loader $load
+ * @property \CI_Log $log
+ * @property \CIX_Model $model
+ * @property \CI_Output $output
+ * @property \CI_Pagination $pagination
+ * @property \CI_Parser $parser
+ * @property \CI_Profiler $profiler
+ * @property \CI_Router $router
+ * @property \CI_Session $session
+ * @property \CI_Sha1 $sha1
+ * @property \CI_Table $table
+ * @property \CI_Trackback $trackback
+ * @property \CI_Typography $typography
+ * @property \CI_Unit_test $unit_test
+ * @property \CI_Upload $upload
+ * @property \CI_URI $uri
+ * @property \CI_User_agent $user_agent
+ * @property \CI_Xmlrpc $xmlrpc
+ * @property \CI_Xmlrpcs $xmlrpcs
+ * @property \CI_Zip $zip
+ * @property \CI_Javascript $javascript
+ * @property \CI_Jquery $jquery
+ * @property \CI_Utf8 $utf8
+ * @property \CI_Security $security
+ */
+
 class Controller extends \CI_Controller
 {
 
